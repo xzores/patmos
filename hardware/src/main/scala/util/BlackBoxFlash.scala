48b6debe283a0e2243c86d50580ba0bc7202be5e
SyedAnasAlam <saa0380@gmail.com>
2022-03-17 17:06:23 +0100

    Added IO for black box flash

--- hardware/src/main/scala/util/BlackBoxFlash.scala
+++ hardware/src/main/scala/util/BlackBoxFlash.scala
@@ -1,10 +1,19 @@
 package util
 
 import chisel3._
+import chisel3.experimental.Analog
 import chisel3.util.HasBlackBoxResource
 
 
 class BlackBoxFlash extends HasBlackBoxResource {
-  val io = IO(new Bundle {})
+  val io = IO(new Bundle {
+    val SI = Analog(Bool())
+    val SO = Analog(Bool())
+    val SCK = Input(Bool())
+    val CSNeg = Input(Bool())
+    val WPNeg = Analog(Bool())
+    val ResetNeg = Analog(Bool())
+
+  })
   addResource("flash-s25fs256.v")
 }
