40b231504a6cc3d72f77d4062f2bcb520d56548b
xzores <jakobfu@hotmail.com>
2022-03-24 09:07:57 +0100

    verilator test updated a bit

--- hardware/src/test/scala/verilator/hello_verilator_test.scala
+++ hardware/src/test/scala/verilator/hello_verilator_test.scala
@@ -1,3 +1,5 @@
+package verilator
+
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
@@ -9,10 +11,15 @@ import hello._
 class hello_verilator_test extends AnyFlatSpec with ChiselScalatestTester
 {
   "hello" should "pass" in {
+    try{
     test(new hello).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
       dut.io.in.poke(false.B)
       dut.clock.step()
       dut.io.out.expect(true.B)
+    }
+    }
+    catch {
+      case e: java.io.IOException => fail(); println("Failed to run varilator test, do you have verilator installed (linux only)");
     }
   }
 }
---
+++ hardware/test_run_dir/hello_should_pass/hello.v
@@ -0,0 +1,60 @@
+module hello(
+  input   clock,
+  input   reset,
+  input   io_in,
+  output  io_out
+);
+`ifdef RANDOMIZE_REG_INIT
+  reg [31:0] _RAND_0;
+`endif // RANDOMIZE_REG_INIT
+  reg  REG; // @[hello.scala 12:22]
+  assign io_out = REG; // @[hello.scala 12:12]
+  always @(posedge clock) begin
+    REG <= ~io_in; // @[hello.scala 12:23]
+  end
+// Register and memory initialization
+`ifdef RANDOMIZE_GARBAGE_ASSIGN
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_INVALID_ASSIGN
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_REG_INIT
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_MEM_INIT
+`define RANDOMIZE
+`endif
+`ifndef RANDOM
+`define RANDOM $random
+`endif
+`ifdef RANDOMIZE_MEM_INIT
+  integer initvar;
+`endif
+`ifndef SYNTHESIS
+`ifdef FIRRTL_BEFORE_INITIAL
+`FIRRTL_BEFORE_INITIAL
+`endif
+initial begin
+  `ifdef RANDOMIZE
+    `ifdef INIT_RANDOM
+      `INIT_RANDOM
+    `endif
+    `ifndef VERILATOR
+      `ifdef RANDOMIZE_DELAY
+        #`RANDOMIZE_DELAY begin end
+      `else
+        #0.002 begin end
+      `endif
+    `endif
+`ifdef RANDOMIZE_REG_INIT
+  _RAND_0 = {1{`RANDOM}};
+  REG = _RAND_0[0:0];
+`endif // RANDOMIZE_REG_INIT
+  `endif // RANDOMIZE
+end // initial
+`ifdef FIRRTL_AFTER_INITIAL
+`FIRRTL_AFTER_INITIAL
+`endif
+`endif // SYNTHESIS
+endmodule
